116fa27326acd7b52fc769d31decb856633aafb6
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-04-02 14:12:55 -0300

    options background and motion added

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,13 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Upload as UploadIcon, ChevronDown, ArrowLeft } from 'lucide-react';
-import Header from '../components/Header';
-import BackToHome from '../components/BackToHome';
-import toast from 'react-hot-toast';
-import { useContent } from '../context/ContentContext';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Upload as UploadIcon,
+  ChevronDown,
+  ArrowLeft,
+  LogIn,
+} from "lucide-react";
+import Header from "../components/Header";
+import BackToHome from "../components/BackToHome";
+import toast from "react-hot-toast";
+import { useContent } from "../context/ContentContext";
 
-type GenerationType = 'background' | 'animation' | null;
-type BackgroundOption = "marble"| "wood"| "industrial"| "linen"| "brick"| "counter" | null;
+type GenerationType = "background" | "animation" | null;
+type BackgroundOption =
+  | "marble"
+  | "wood"
+  | "industrial"
+  | "linen"
+  | "brick"
+  | "counter"
+  | "liso"
+  | null;
 
 export default function Upload() {
   const [file, setFile] = useState<File | null>(null);
@@ -18,14 +31,14 @@ export default function Upload() {
   const [showAnimationOptions, setShowAnimationOptions] = useState(false);
   const [selectedOption, setSelectedOption] = useState<BackgroundOption>(null);
   const [selectAnimation, setSelectAnimation] = useState(false);
-  const {setContent} = useContent();
+  const { setContent } = useContent();
   const navigate = useNavigate();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
       setFile(selectedFile);
-      setIsVideo(selectedFile.type.startsWith('video/'));
+      setIsVideo(selectedFile.type.startsWith("video/"));
       const previewUrl = URL.createObjectURL(selectedFile);
       setPreview(previewUrl);
       resetOptions();
@@ -36,55 +49,81 @@ export default function Upload() {
     if (!file) return;
 
     const formData = new FormData();
-    formData.append('image', file);
-    
+    formData.append("image", file);
 
     try {
-      const response = await fetch('https://72jdmlb6-3500.brs.devtunnels.ms/upload-image', {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token') || localStorage.getItem('devToken')}`,
-        },
-        
-       
-      });
+      const response = await fetch(
+        "https://72jdmlb6-3500.brs.devtunnels.ms/upload-image",
+        {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer ${
+              localStorage.getItem("token") || localStorage.getItem("devToken")
+            }`,
+          },
+        }
+      );
 
-      if (!response.ok) throw new Error('Upload failed');
+      if (!response.ok) throw new Error("Upload failed");
 
       const data = await response.json();
       const fileName = data.fileName; // Assuming the response contains the file name
-      
 
-      toast.success('Archivo subido exitosamente');
+      console.log("fileName", fileName);
+
+      toast.success("Archivo subido exitosamente");
       try {
-        if(!fileName) throw new Error('File name not found');
-        const response = await fetch(`https://72jdmlb6-3500.brs.devtunnels.ms/remove-background`, {
-          method: 'POST',
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token') || localStorage.getItem('devToken')}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            fileName,
-            background: selectedOption,
-            animation: selectAnimation,
-          }),
-        })
-        const {result_url} = await  response.json()
+        if (!fileName) throw new Error("File name not found");
+        console.log("animation", selectAnimation);
+        console.log("background", selectedOption);
+        const timeout = 120000; // 2 minutos en milisegundos
+
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+        const response = await fetch(
+          `https://72jdmlb6-3500.brs.devtunnels.ms/remove-background`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${
+                localStorage.getItem("token") ||
+                localStorage.getItem("devToken")
+              }`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              fileName,
+              background: selectedOption === "liso" ? null : selectedOption,
+              animation: selectAnimation,
+            }),
+            signal: controller.signal,  // Asociar el AbortController con la solicitud
+          }
+        );
+
+        clearTimeout(timeoutId);  // Limpiar el timeout si la solicitud es exitosa
+        const { result_url, type } = await response.json();
+        console.log("result_url", result_url);
+        console.log("type", type);
+
         setContent({
           id: data.id,
           url: result_url,
-          type: isVideo ? 'video' : 'image',
+          type: type,
         });
-        
-      } catch (error) {
-        
+        navigate(`/view/${data.id}`);
+      } catch (error:any) {
+        if (error.name === 'AbortError') {
+          console.error('La solicitud fue cancelada por un timeout');
+        } else {
+          console.error('Error en la solicitud', error);
+        }
+        toast.error("Error al eliminar el fondo");
       }
-      navigate(`/view/${data.id}`);
     } catch (error) {
-      toast.error('Error al subir el archivo');
-      console.error('Upload error:', error);
+      toast.error("Error al subir el archivo");
+      console.error("Upload error:", error);
     }
   };
   const resetOptions = () => {
@@ -102,21 +141,19 @@ export default function Upload() {
 
   const handleGenerationTypeSelect = (type: GenerationType) => {
     setGenerationType(type);
-    if (type === 'background') {
+    if (type === "background") {
       setShowBackgroundOptions(!showBackgroundOptions);
       setShowAnimationOptions(false);
-    } else if (type === 'animation') {
+    } else if (type === "animation") {
       setShowAnimationOptions(!showAnimationOptions);
       setShowBackgroundOptions(false);
     }
   };
 
- 
-
   return (
     <div className="min-h-screen bg-gray-900">
       <Header />
-      
+
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 ">
         <div className="px-4 py-6 sm:px-0">
           <div className="border-4 border-dashed border-gray-700 rounded-lg p-12">
@@ -156,11 +193,7 @@ export default function Upload() {
               <div className="mt-8 max-w-md mx-auto">
                 <div className="bg-gray-800 rounded-lg shadow-lg ">
                   {isVideo ? (
-                    <video
-                      src={preview}
-                      controls
-                      className="w-full h-auto"
-                    />
+                    <video src={preview} controls className="w-full h-auto" />
                   ) : (
                     <img
                       src={preview}
@@ -173,48 +206,58 @@ export default function Upload() {
                       <div className="space-y-3 ">
                         <div className="relative">
                           <button
-                            onClick={() => handleGenerationTypeSelect('background')}
+                            onClick={() =>
+                              handleGenerationTypeSelect("background")
+                            }
                             className="w-full flex justify-between items-center bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
                           >
                             <span>Generar Fondo</span>
                             <ChevronDown size={20} />
                           </button>
-                          
+
                           {showBackgroundOptions && (
                             <div className="absolute w-full mt-1 bg-gray-700 rounded-md shadow-lg z-10 ">
                               <div className="py-1">
                                 <button
-                                  onClick={() => handleOptionSelect('marble')}
+                                  onClick={() => handleOptionSelect("liso")}
+                                  className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
+                                >
+                                  Liso
+                                </button>
+                                <button
+                                  onClick={() => handleOptionSelect("marble")}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Mármol
                                 </button>
                                 <button
-                                  onClick={() => handleOptionSelect('wood')}
+                                  onClick={() => handleOptionSelect("wood")}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Madera
                                 </button>
                                 <button
-                                  onClick={() => handleOptionSelect('industrial')}
+                                  onClick={() =>
+                                    handleOptionSelect("industrial")
+                                  }
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Industrial
                                 </button>
                                 <button
-                                  onClick={() => handleOptionSelect('linen')}
+                                  onClick={() => handleOptionSelect("linen")}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Lino
                                 </button>
                                 <button
-                                  onClick={() => handleOptionSelect('brick')}
+                                  onClick={() => handleOptionSelect("brick")}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Ladrillo
                                 </button>
                                 <button
-                                  onClick={() => handleOptionSelect('counter')}
+                                  onClick={() => handleOptionSelect("counter")}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Mostrador
@@ -226,61 +269,78 @@ export default function Upload() {
 
                         <div className="relative">
                           <button
-                            onClick={() => handleGenerationTypeSelect('animation')}
+                            onClick={() =>
+                              handleGenerationTypeSelect("animation")
+                            }
                             className="w-full flex justify-between items-center bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition"
                           >
                             <span>Generar Fondo + Animación</span>
                             <ChevronDown size={20} />
                           </button>
-                          
+
                           {showAnimationOptions && (
                             <div className="absolute w-full mt-1 bg-gray-700 rounded-md shadow-lg z-10">
                               <div className="py-1">
-                              <button
+                                <button
                                   onClick={() => {
-                                    handleOptionSelect('marble') 
-                                    setSelectAnimation(true)} }
+                                    handleOptionSelect("liso");
+                                    setSelectAnimation(true);
+                                  }}
+                                  className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
+                                >
+                                  Liso
+                                </button>
+                                <button
+                                  onClick={() => {
+                                    handleOptionSelect("marble");
+                                    setSelectAnimation(true);
+                                  }}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Mármol
                                 </button>
                                 <button
-                                  onClick={() =>{ 
-                                    handleOptionSelect('wood')
-                                    setSelectAnimation(true)}}
+                                  onClick={() => {
+                                    handleOptionSelect("wood");
+                                    setSelectAnimation(true);
+                                  }}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Madera
                                 </button>
                                 <button
-                                  onClick={() =>{ 
-                                    handleOptionSelect('industrial')
-                                    setSelectAnimation(true)}}
+                                  onClick={() => {
+                                    handleOptionSelect("industrial");
+                                    setSelectAnimation(true);
+                                  }}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Industrial
                                 </button>
                                 <button
-                                  onClick={() =>{ handleOptionSelect('linen')
-                                    setSelectAnimation(true)
+                                  onClick={() => {
+                                    handleOptionSelect("linen");
+                                    setSelectAnimation(true);
                                   }}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Lino
                                 </button>
                                 <button
-                                  onClick={() =>{ handleOptionSelect('brick')
+                                  onClick={() => {
+                                    handleOptionSelect("brick");
 
-                                    setSelectAnimation(true)
+                                    setSelectAnimation(true);
                                   }}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
                                   Ladrillo
                                 </button>
                                 <button
-                                  onClick={() => {handleOptionSelect('counter')
+                                  onClick={() => {
+                                    handleOptionSelect("counter");
 
-                                    setSelectAnimation(true)
+                                    setSelectAnimation(true);
                                   }}
                                   className="w-full text-left px-4 py-2 text-white hover:bg-gray-600"
                                 >
@@ -315,7 +375,7 @@ export default function Upload() {
           </div>
         </div>
       </main>
-      
+
       <BackToHome />
     </div>
   );
